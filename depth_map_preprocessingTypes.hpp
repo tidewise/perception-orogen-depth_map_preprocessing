c1755a016736a59e697891a5f22893596fd1041e
Sascha Arnold <sascha.arnold@dfki.de>
2016-09-26 11:29:43 +0200

    added documentation for the MotionCompensation enum

--- depth_map_preprocessingTypes.hpp
+++ depth_map_preprocessingTypes.hpp
@@ -11,6 +11,21 @@
 namespace depth_map_preprocessing
 {
 
+/**
+ * :NoCompensation
+ *    No motion compensation will be applyed.
+ * :HorizontalInterpolation and :VerticalInterpolation
+ *    Will interpolate the local movement by the first and the last timestamp
+ *    and will apply the transformations either horizontal (column-wise) or
+ *    vertical (row-wise) in the frame of the newest scan.
+ *    Note that |depth_map.timestamps| >= 2.
+ * :Horizontal and :Vertical
+ *    Will get one transformation for each timestamp in depth_map.timestamps
+ *    and will apply them in the frame of the newest scan.
+ *    Note that |depth_map.timestamps| == |depth_map.horizontal_size| for the
+ *    horizontal case and |depth_map.timestamps| == |depth_map.vertical_size|
+ *    for the vertical case.
+ */
 enum MotionCompensation
 {
     NoCompensation = 0,
